6ff12157648565cdd6af17f205bd9c4a0648536f
mabuhanifa <moh.abuhanifa@gmail.com>
2023-09-03 00:18:25 +0600

    node_modules removed

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 
 const app = express();
 
-app.get('/',(req,res)=>{
-res.send({
-    message:"Hello from express api"
-})
-})
+app.get("/", (req, res) => {
+  res.send({
+    message: "Hello from express api",
+  });
+});
 
-app.listen(port,()=>{
-    console.log(`App running on port ${port}`)
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
