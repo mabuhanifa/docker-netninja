6ef98619e0c0f65314eba685585f23ed45949f8a
mabuhanifa <moh.abuhanifa@gmail.com>
2023-09-03 00:20:12 +0600

    file updated

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const port = process.env.PORT || 3000;
 const cors = require("cors");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send({
